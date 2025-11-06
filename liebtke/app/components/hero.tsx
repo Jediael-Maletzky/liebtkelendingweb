9ce5e62f773b027c99e95f9fc9b7258d0b16ce08
Jediael Maletzky <maletzkyjsr@gmail.com>
2025-11-06 22:03:17 +0200

    'HeroandNav'

--- liebtke/app/components/hero.tsx
+++ liebtke/app/components/hero.tsx
@@ -1,12 +1,13 @@
 export default function Hero() {
     return (
-        <section className="grid-cols-2 items-center justify-center min-h-dvh bg-black-100">
-            <div className="flex justify-center items-center">
+        <div className="flex h- bg-black-100">
+            <div className="flex justify-center items-center h-full flex-col text-center">
                 <h1 className="text-4xl font-bold">Welcome to Liebtke Lending</h1>
             </div>
             <div className="flex justify-center items-center">
                 <p className="mt-4 text-xl">Your Trusted Financial Partner</p>
             </div>
-        </section>
+
+        </div>
     );
 }
--- liebtke/app/components/navbar.tsx
+++ liebtke/app/components/navbar.tsx
@@ -1,16 +1,26 @@
-import Image from 'next/image';
-import logo from '@/public/LOGO PNG.png';
-
 export default function Navbar() {
   return (
-    <nav className="flex items-center p-2 bg-white text-gray-800">
-      <Image 
-        src={logo} 
-        alt="Liebtke Lending Logo"
-        width={100}
-        height={50}
-      />
-      <h2 className="ml-4">Liebtke Lending</h2>
+    <nav className="bg-transparent" aria-label="Main Navigation">
+      <div className= "flex display-inline wrap justify-between items-center p-2 bg-gray-800 text-white">
+        <div>
+            <img src="/LOGO PNG.png" alt="Liebtke Lending Logo" className="h-15 w-50"/>
+        </div>
+
+        <div>
+          <ul>
+          <li className="inline-block mx-4"><a href="#home" className="hover:">Home</a></li>
+          <li className="inline-block mx-4"><a href="#about" className="hover:underline">How it works</a></li>
+          <li className="inline-block mx-4"><a href="#services" className="hover:underline">Services</a></li>
+          <li className="inline-block mx-4"><a href="#contact" className="hover:underline">Contact</a></li>
+        </ul>
+        </div>
+
+        <div>
+
+          <button className="hover:transform text-white font-bold py-2 px-4 rounded">Log In </button>
+        </div>
+        
+      </div>
     </nav>
-  );
+  ); 
 }
